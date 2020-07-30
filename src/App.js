fa3b5e1c91b4ac7c33729ca9eb3286a2d6346324
softchris <noringc@gmail.com>
2020-07-30 12:43:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Main from './Main';
+import Head from './Head';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">Products Inventory App</div>
+      <Head />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Products.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './products.css';
+
+async function getProducts() {
+  try {
+    const res = await fetch('/api/Products')
+    console.log(res.status)
+    console.log(res.statusText)
+    const products = await res.json()
+    return products
+  } catch (err) {
+    throw err.message;
+  }
+}
+
+class Products extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      products: []
+    };
+  }
+
+  async componentDidMount() {
+    const products = await getProducts();
+    this.setState({ products })
+  }
+
+  render() {
+    return (
+    <div>
+      {this.state.products.map(p => <div key={p.id} className="item">{p.name}</div>)}
+    </div>
+    )
+  }
+}
+
+export default Products
